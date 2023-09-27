c3a738db9c1df49fa19606474b8a1907cadfc358
EthanGilles <ehgilles01@gmail.com>
2023-09-27 14:25:04 -0400

    myStack implementations

--- src/defaultpackage/myStack.java
+++ src/defaultpackage/myStack.java
@@ -1,5 +1,33 @@
 package defaultpackage;
 
-public class myStack {
+import java.util.ArrayList;
+
+public class myStack implements IStack{
+
+	private ArrayList<String> list;
+	
+	public myStack() {
+		list = new ArrayList<>();
+	}
+	
+	public void push(String c) {
+		list.add(0, c);
+	}
+
+	public String pop() {
+		return list.remove(0);
+	}
+
+	public String peek() {
+		return list.get(0);
+	}
+
+	public boolean isEmpty() {
+		return list.size() == 0;
+	}
+	
+	public int size() {
+		return list.size();
+	}
 
 }
